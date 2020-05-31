cf27205b1af89dc755e0006123b9475a5b287e86
Robert Shenton <theshenton@gmail.com>
2020-05-31 22:19:22 +0200

    Upgrade packages

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ var $$ = {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(28:3) `@apply` cannot be used with .transition because .transition is included in multiple rulesets.\n\n  26 | \n  27 | a {\n> 28 |   @apply text-purple-900 no-underline transition;\n     |   ^\n  29 | }\n  30 | \n\n    at C:\\Users\\Admin\\Documents\\Dev\\robert\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\Admin\\Documents\\Dev\\robert\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\Admin\\Documents\\Dev\\robert\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\Admin\\Documents\\Dev\\robert\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Users\\Admin\\Documents\\Dev\\robert\\node_modules\\postcss-loader\\src\\index.js:207:9");
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(28:3) `@apply` cannot be used with .transition because .transition is included in multiple rulesets.\n\n \u001b[90m 26 | \u001b[39m\n \u001b[90m 27 | \u001b[39ma \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 28 | \u001b[39m  \u001b[36m@apply\u001b[39m text-purple-900 no-underline transition\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 29 | \u001b[39m\u001b[33m}\u001b[39m\n \u001b[90m 30 | \u001b[39m\n\n    at /Users/robertshenton/Documents/Dev/robert/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/robertshenton/Documents/Dev/robert/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/robertshenton/Documents/Dev/robert/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/robertshenton/Documents/Dev/robert/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Users/robertshenton/Documents/Dev/robert/node_modules/postcss-loader/src/index.js:207:9");
 
 /***/ }),
 
@@ -245,8 +245,8 @@ throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Admin\Documents\Dev\robert\resources\js\main.js */"./resources/js/main.js");
-module.exports = __webpack_require__(/*! C:\Users\Admin\Documents\Dev\robert\resources\sass\main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! /Users/robertshenton/Documents/Dev/robert/resources/js/main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! /Users/robertshenton/Documents/Dev/robert/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
